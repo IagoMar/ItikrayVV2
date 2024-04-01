252a389759ba589e5e9e904d15a7a2e3a89b1080
iago <iagomzzh@gmail.com>
2024-04-02 00:24:39 +0200

    The preliminar version(PrincipalPage) is finished

--- app/src/main/java/tikray/iago/tikray2v/screens/principalPage.kt
+++ app/src/main/java/tikray/iago/tikray2v/screens/principalPage.kt
@@ -6,24 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.simulateHotReload
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import tikray.iago.tikray2v.ui.theme.Tikray2VTheme
-
 import tikray.iago.tikray2v.R
 
 class MainActivity : ComponentActivity() {
@@ -47,38 +43,58 @@ fun ScreenRegister() {
     ) {
 
         // Valores para la colocacion de elemntos relativos
-        val (logo, buttonLogin, buttonSignUp) = createRefs() // Creamos las referencias
-        val topMargin = createGuidelineFromTop(0.3f) // Creamos el margen de arriba
+        val (title, logo, buttonLogin, buttonSignUp) = createRefs() // Creamos las referencias
+        val topMargin = createGuidelineFromTop(0.1f) // Creamos el margen de arriba Para el titulo
+        val topMargin1 = createGuidelineFromTop(0.3f) // Creamos el margen de arriba Para el logo
+
+
+
+        Text(
+            text = "TIKRAY", modifier = Modifier
+                .constrainAs(title) {
+                    top.linkTo(topMargin)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 70.sp, color = Color.White,
+
+                )
+        )
 
         Image(
             painter = painterResource(id = R.drawable.logo_empresa),
             contentDescription = "logo company", modifier = Modifier.constrainAs(logo) {
-                top.linkTo(topMargin)
+                top.linkTo(topMargin1)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }
         )
 
-        Button(onClick = { /*TODO*/ }, modifier = Modifier
-            .width(120.dp)
-            .constrainAs(buttonLogin) {
-                top.linkTo(logo.bottom, margin = 75.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
+        Button(
+            onClick = { /*TODO*/ }, modifier = Modifier
+                .width(120.dp)
+                .constrainAs(buttonLogin) {
+                    top.linkTo(logo.bottom, margin = 75.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
 
 
+                },
 
-            },
-            shape = CutCornerShape(3.dp)) {
+            shape = CutCornerShape(3.dp)
+        ) {
             Text(text = "LOGIN")
 
         }
-        Button(onClick = { /*TODO*/ }, modifier = Modifier
-            .width(120.dp)
-            .constrainAs(buttonSignUp) {
-                top.linkTo(buttonLogin.bottom, margin = 5.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end) },
+        Button(
+            onClick = { /*TODO*/ }, modifier = Modifier
+                .width(120.dp)
+                .constrainAs(buttonSignUp) {
+                    top.linkTo(buttonLogin.bottom, margin = 5.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
             shape = CutCornerShape(3.dp)
         ) {
             Text(text = "SIGN UP")
@@ -95,7 +111,6 @@ fun ScreenRegister() {
 @Preview(showSystemUi = true)
 @Composable
 fun Preview() {
-
     ScreenRegister()
 
 
