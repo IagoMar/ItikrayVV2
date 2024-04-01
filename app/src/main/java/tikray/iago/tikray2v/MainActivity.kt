ac2aefebbd1a9db3fa3ccd54801b4ba2ed705644
iago <iagomzzh@gmail.com>
2024-04-01 20:20:59 +0200

    ssa

--- app/src/main/java/tikray/iago/tikray2v/MainActivity.kt
+++ app/src/main/java/tikray/iago/tikray2v/MainActivity.kt
@@ -3,13 +3,17 @@ package tikray.iago.tikray2v
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 import tikray.iago.tikray2v.ui.theme.Tikray2VTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +24,37 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(19, 18, 69)
                 ) {
-                    Greeting()
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting() {
-    Text(
-        text = "Hello IAgo!",
 
-    )
+@Composable
+fun ScreenRegister(modifier: Modifier) {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+        
+
+    }
+
+
+
 }
 
-@Preview(showBackground = true)
+
+
+
+
+
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    Tikray2VTheme {
-        Greeting()
-    }
+fun Preview() {
+    ScreenRegister(Modifier.background(Color(19, 18, 69)))
+
+
 }
